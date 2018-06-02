d8509b7cc0c45a6d3ec82e545fc2cec2bee2eefd
konstantinp <konstantin.poklonskiy@arcadia.spb.ru>
2018-06-02 14:43:34 +0300

    add migration runner

---
+++ CqrsApi/CqrsApi.Migrations/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using CqrsApi.Migrations.Migrations;
+using FluentMigrator.Runner;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CqrsApi.Migrations
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceProvider = CreateServices();
+
+            // Put the database update into a scope to ensure
+            // that all resources will be disposed.
+            using (var scope = serviceProvider.CreateScope())
+            {
+                UpdateDatabase(scope.ServiceProvider);
+            }
+        }
+
+        /// <summary>
+        /// Configure the dependency injection services
+        /// </sumamry>
+        private static IServiceProvider CreateServices()
+        {
+            return new ServiceCollection()
+                // Add common FluentMigrator services
+                .AddFluentMigratorCore()
+                .ConfigureRunner(rb => rb
+                    // Add SQLite support to FluentMigrator
+                    .AddSqlServer2014()
+                    // Set the connection string
+                    .WithGlobalConnectionString("Data Source=(local);Initial Catalog=CqrsApi;Integrated Security=true;")
+                    // Define the assembly containing the migrations
+                    .ScanIn(typeof(Migration201802061334).Assembly).For.Migrations())
+                // Enable logging to console in the FluentMigrator way
+                .AddLogging(lb => lb.AddFluentMigratorConsole())
+                // Build the service provider
+                .BuildServiceProvider(false);
+        }
+
+        /// <summary>
+        /// Update the database
+        /// </sumamry>
+        private static void UpdateDatabase(IServiceProvider serviceProvider)
+        {
+            // Instantiate the runner
+            var runner = serviceProvider.GetRequiredService<IMigrationRunner>();
+
+            // Execute the migrations
+            runner.MigrateUp();
+        }
+    }
+}
