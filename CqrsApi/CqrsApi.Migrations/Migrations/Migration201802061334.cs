8cf2134805895c4028bb163d5a337580aae5add9
konstantinp <konstantin.poklonskiy@arcadia.spb.ru>
2018-06-02 13:47:59 +0300

    added initial migration for database

---
+++ CqrsApi/CqrsApi.Migrations/Migrations/Migration201802061334.cs
@@ -0,0 +1,22 @@
+﻿using FluentMigrator;
+
+namespace CqrsApi.Migrations.Migrations
+{
+    [Migration(201802061334)]
+    public class Migration201802061334 : ForwardOnlyMigration
+    {
+        public override void Up()
+        {
+            Create.Table("Customers")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Name").AsString(255).NotNullable()
+                .WithColumn("Email").AsString(255).NotNullable().Unique("Customers_Email_Uniq");
+
+            Create.Table("Orders")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("Price").AsDecimal(19, 4).NotNullable()
+                .WithColumn("CreatedDate").AsDateTime().WithDefaultValue(SystemMethods.CurrentUTCDateTime)
+                .WithColumn("CustomerId").AsInt32().ForeignKey("FK_Orders_Customers", "Customers", "Id");
+        }
+    }
+}
