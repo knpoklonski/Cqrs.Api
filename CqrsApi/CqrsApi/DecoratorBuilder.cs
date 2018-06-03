42abd68fbf1c3fd17c760cbc827e8be966cf7dbc
konstantinp <konstantin.poklonskiy@arcadia.spb.ru>
2018-06-03 18:37:00 +0300

    add infrastructure for decorator

---
+++ CqrsApi/CqrsApi/DecoratorBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CqrsApi
+{
+    public class DecoratorBuilder<TInterface> where TInterface : class
+    {
+        private readonly IServiceCollection _services;
+        private TInterface _commandHandler;
+
+        public DecoratorBuilder(IServiceCollection services)
+        {
+            _services = services;
+        }
+
+        public DecoratorBuilder<TInterface> Default<TService>()
+            where TService : class, TInterface
+        {
+            _services.AddTransient<TService>();
+            var provider = _services.BuildServiceProvider();
+            _commandHandler = (TInterface) provider.GetService(typeof(TService));
+
+            return this;
+        }
+
+        public DecoratorBuilder<TInterface> Envelop(Func<IServiceProvider, TInterface, TInterface> envelop)
+        {
+            var provider = _services.BuildServiceProvider();
+            _commandHandler = envelop(provider, _commandHandler);
+            return this;
+        }
+
+        public DecoratorBuilder<TInterface> Register()
+        {
+            _services.AddTransient(provider => _commandHandler);
+            return this;
+        }
+    }
+}
---
+++ CqrsApi/CqrsApi/ServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CqrsApi
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static DecoratorBuilder<TInterface> DecoratorFor<TInterface>(this IServiceCollection services) where TInterface : class
+        {
+            return new DecoratorBuilder<TInterface>(services);
+        }
+    }
+}
