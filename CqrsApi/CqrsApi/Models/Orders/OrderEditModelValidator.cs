159b84b5b6e73b87f29d7d688496e544f3314020
konstantinp <konstantin.poklonskiy@arcadia.spb.ru>
2018-06-10 15:56:42 +0300

    add validation for price field

---
+++ CqrsApi/CqrsApi/Models/Orders/OrderEditModelValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace CqrsApi.Models.Orders
+{
+    public class OrderEditModelValidator : AbstractValidator<OrderEditModel>
+    {
+        public OrderEditModelValidator()
+        {
+            RuleFor(m => m.Price).GreaterThan(0);
+        }
+    }
+}
